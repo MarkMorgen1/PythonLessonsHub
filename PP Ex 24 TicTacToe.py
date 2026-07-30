5e1c6e3c4a1a1d81cb9f83158b3479d43a0f074a
MarkMorgen <markm63816@gmail.com>
2026-07-29 19:56:14 -0700

    July 29 2026

--- PP Ex 24 TicTacToe.py
+++ PP Ex 24 TicTacToe.py
@@ -1,9 +1,10 @@
 # w = int(input("Enter grid width: "))
 # h = int(input("Enter grid height: "))
 w = 3
-h = 3
+h = w  # create a square grid for game
 turns_taken = []  # use to keep track of each turn already taken
-game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # use this matrix to test for wins later
+# use this matrix to test for wins later
+game = [[0 for col in range(w)] for row in range(h)]
 game_over = False
 print("Let's play some TicTacToe!")
 # fill and print the grid using zeros
@@ -66,43 +67,60 @@ while not game_over:
 
     #        print("| 0 " + "|") # using '+' eliminates the space created when using the ',' in a print statement
 
-    # check for a vertical win using 'game' matrix
+    # check for a horizontal win using 'game' matrix
     print("Game Matrix:", game)
-    for i in range(3):
-        if game[i][0] * game[i][1] * game[i][2] == 1:
+    for j in range(h):
+        horz = 1
+        for i in range(w):
+            horz *= game[i][j]
+        if horz == 1:
             print("Player X wins !!!\n")
             game_over = True
-        elif game[i][0] * game[i][1] * game[i][2] == 8:
+        elif horz == 2**w:
             print("Player Y wins !!!\n")
             game_over = True
 
-    # check for a horizontal win
+    # check for a vertical win
     if not game_over:
-        for j in range(3):
-            if game[0][j] * game[1][j] * game[2][j] == 1:
+        for i in range(h):
+            vert = 1
+            for j in range(w):
+                vert *= game[i][j]
+            if vert == 1:
                 print("Player X wins !!!\n")
                 game_over = True
-            elif game[0][j] * game[1][j] * game[2][j] == 8:
+            elif vert == 2**w:
                 print("Player Y wins !!!\n")
                 game_over = True
 
-    # check for a diagonal win
+    # check for a diagonal win if game is still active
     if not game_over:
-        if (game[0][0] * game[1][1] * game[2][2] == 1) or (
-            game[0][2] * game[1][1] * game[2][0] == 1
-        ):
-            print("Player X wins !!!\n")
+        diag1 = 1
+        for i in range(w):
+            diag1 *= game[i][i]
+        if diag1 == 1:
+            print("Diagonal X wins!\n")
+            game_over = True
+        elif diag1 == 2**w:
+            print("Diagonal Y wins!\n")
             game_over = True
 
+    # check other diagonal for a win
     if not game_over:
-        if (game[0][0] * game[1][1] * game[2][2] == 8) or (
-            game[0][2] * game[1][1] * game[2][0] == 8
-        ):
-            print("Player Y wins !!!\n")
+        diag2 = 1
+        for i in range(w):
+            diag2 *= game[i][w - 1 - i]
+        if diag2 == 1:
+            print("Diagonal X wins!\n")
+            game_over = True
+        elif diag2 == 2**w:
+            print("Diagonal Y wins!\n")
             game_over = True
 
-    if not game_over and len(turns_taken) == 9:  # no one has won after 9 tries
+    if (
+        not game_over and len(turns_taken) == w * h
+    ):  # no one has won after the matrix is full
         game_over = True
         print("Game is a draw.  Restart to go again.\n")
-    elif not game_over and len(turns_taken) < 9:
+    elif not game_over and len(turns_taken) < w * h:
         print("No winner yet...\n")
--- PP Ex 33 Birthday Dictionaries.py
+++ PP Ex 33 Birthday Dictionaries.py
@@ -1,6 +1,9 @@
 import json
 import calendar
 from collections import Counter
+from bokeh.plotting import figure, show, output_file
+from bokeh.io import output_notebook
+from bokeh.models import ColumnDataSource
 
 # Note - JSON data does not name the file, so bdays.JSON is not the same data structure as 'bdays' the dictionary within the Python code
 # bdays = {"Mark": "11/28/1966", "Joe": "1/2/1988", "Cindy": "8/15/2024"}
@@ -11,9 +14,8 @@ from collections import Counter
 
 # read the JSON file to get the stored data
 with open("bdays.json", "r") as f:
-    bdays = json.load(
-        f
-    )  # redefines the bdays dictionary in Python code from external JSON file
+    bdays = json.load(f)
+    # defines the bdays dictionary in Python code from external JSON file
 
 print("JSON Info:", bdays)  # verify the file
 # bdays["Sally"] = "3/4/1975"  # for debugging -- add new entry to bdays dictionary in Python
@@ -52,13 +54,13 @@ else:  # write new bday to bdays.json file
 # Values: 'Counter' counts the number of times each month number occurs
 # use .get(i,0) to safely return a 0 if no birthdays in that month
 # shows all 12 months
-month_counts = {
+all_months = {
     calendar.month_name[i]: Counter(
         int(date.split("/")[0]) for date in bdays.values()
     ).get(i, 0)
     for i in range(1, 13)
 }
-print(month_counts, "\n")
+print(all_months, "\n")
 
 
 # only prints months with birthdays in chronilogical order
@@ -71,3 +73,28 @@ month_counts = {
     if month_counter[m] > 0
 }
 print(month_counts, "\n")
+
+
+# PLOTTING
+output_file("plot.html")
+months_of_year = list(all_months.keys())
+print(months_of_year)
+counts_by_month = list(all_months.values())
+print(counts_by_month)
+
+# create a figure from bday info
+p = figure(
+    x_range=months_of_year,
+    height=600,
+    title="BDays per Month",
+    toolbar_location=None,
+    tools="",
+)
+# create a histogram (vertical bar = vbar)
+p.vbar(x=months_of_year, top=counts_by_month, width=0.8)
+p.xgrid.grid_line_color = None
+p.legend.orientation = "horizontal"
+p.legend.location = "top_center"
+p.y_range.start = 0
+# render the plot
+show(p)
